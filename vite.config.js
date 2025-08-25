bd91434725fa483586ae7d13d7b3a01ab082f19a
jinshuanyu <shuanshuan@gmail.com>
2025-08-25 22:32:59 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vowel-detective/',   // 一定要這個，才不會空白
+  build: { target: ['es2017', 'safari13'] }
 })
